28409a87c5a0b6c25a91fb2f026902a702ec6c3d
Aaron Frase <aaron@rvshare.com>
2017-11-23 14:51:43 -0500

    Report code coverage to CodeClimate

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,6 +1,13 @@
 # frozen_string_literal: true
 
 require 'simplecov'
+
+# save to CircleCI's artifacts directory if we're on CircleCI
+if ENV['CIRCLE_ARTIFACTS']
+  dir = File.join(ENV['CIRCLE_ARTIFACTS'], 'coverage')
+  SimpleCov.coverage_dir(dir)
+end
+
 SimpleCov.start
 
 require 'firebug'
